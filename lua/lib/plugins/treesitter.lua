c7f50c382dd1e39c7e2cff087a9c4712ab1a742d
sig1nt <sigint@scriptalert.one>
2026-03-29 15:32:59 -0700

    Treesitter re-write hack

--- lua/lib/plugins/treesitter.lua
+++ lua/lib/plugins/treesitter.lua
@@ -1,2 +1,4 @@
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
+-- HACK: For some reason we have the queries inside of a "runtime" directory that vim.treesitter doesn't expect
+vim.opt.runtimepath:append(vim.pack.get({'nvim-treesitter'})[1].path .. '/runtime')
 require('nvim-treesitter').install({ 'python', 'javascript', 'typescript', 'go', 'ruby', 'lua', 'markdown', 'rust', 'json', 'make', 'swift'})
